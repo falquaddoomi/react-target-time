e0c7cabc2cc732da537c9002cbccd08b1ca19efc
Faisal <falquaddoomi@ucla.edu>
2017-07-21 23:36:32 +0200

    Webpack now builds a bundle file, testing to see if that's what I want...

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,42 @@
+/* globals module */
 module.exports = {
-  entry: './demo/index.js',
-  output: {
-    path: './demo',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
-      { test: /\.s?css$/, loaders: ["style", "css", "sass"] }
+    entry: './src/TargetTime/index.js',
+    output: {
+        libraryTarget: 'umd',
+        library: 'TargetTime',
+        path: './dist/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
+            { test: /\.s?css$/, loaders: ["style", "css", "sass"] }
+        ]
+    },
+    externals: [
+        {
+            'react-dom': {
+                root: 'ReactDOM',
+                commonjs2: 'react-dom',
+                commonjs: 'react-dom',
+                amd: 'react-dom'
+            }
+        },
+        {
+            'react': {
+                root: 'React',
+                commonjs2: 'react',
+                commonjs: 'react',
+                amd: 'react'
+            }
+        },
+        {
+            'moment': {
+                root: 'moment',
+                commonjs2: 'moment',
+                commonjs: 'moment',
+                amd: 'moment'
+            }
+        },
     ]
-  }
 };
